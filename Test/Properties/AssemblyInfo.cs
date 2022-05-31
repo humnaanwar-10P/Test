b2f332cae693b89a1929ad166891e3a4ebf11d1a
humna.anwar <humna.anwar@HUMNAANWER>
2022-05-31 14:42:58 +0500

    Add project files.

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("10pearls")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © 10pearls 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abbdd18d-029f-47ef-8e3e-d2c76b90c7ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        IWebDriver driver = new ChromeDriver();
+
+
+        [TestMethod]
+        public void OpenUrl()
+        {
+            driver.Navigate().GoToUrl("https://tifis.qpharmasit.com");
+        }
+    }
+}
